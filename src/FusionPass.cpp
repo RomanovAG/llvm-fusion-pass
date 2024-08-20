f7f9791ae6be474f4c46b7b47652231af0e3b18b
RomanovAG <andpey.romanov@gmail.com>
2024-08-20 16:39:57 +0300

    added control-flow eq sets

--- src/FusionPass.cpp
+++ src/FusionPass.cpp
@@ -1,6 +1,6 @@
-#include <functional>
 #include "llvm/IR/Dominators.h"
-//#include "llvm/IR/InstIterator.h"
+#include "llvm/Analysis/PostDominators.h"
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
@@ -40,6 +40,39 @@ loopContainsVolatileInst(const Loop &L)
 }
 
 bool
+loopDominates(Loop *L1, Loop *L2, DominatorTree &DT)
+{
+	//SmallVector<BasicBlock *> L1ExitBBs, L2ExitBBs;
+	//L1->getExitingBlocks(L1ExitBBs);
+	//L2->getExitingBlocks(L2ExitBBs);
+	//outs() << L1ExitBBs.size() << " " << L2ExitBBs.size() << "\n";
+	const auto H1 = L1->getLoopPreheader();
+	const auto H2 = L2->getLoopPreheader();
+	return DT.properlyDominates(H1, H2);
+	/*for (BasicBlock *BB1 : L1ExitBBs)
+	{
+		for (BasicBlock *BB2 : L2ExitBBs)
+		{
+			if (DT.properlyDominates(BB1, BB2) == false)
+			{
+				outs() << "NO\n";
+				return false;
+			}
+		}
+	}
+	return true;*/
+}
+
+bool
+loopPostDominates(Loop *L1, Loop *L2, PostDominatorTree &PDT)
+{
+	const auto H1 = L1->getLoopPreheader();
+	const auto H2 = L2->getLoopPreheader();
+	return PDT.properlyDominates(H1, H2);
+
+}
+
+bool
 FuseLoops(Function &F, FunctionAnalysisManager &AM)
 {
 	
@@ -48,14 +81,63 @@ FuseLoops(Function &F, FunctionAnalysisManager &AM)
 	//li.print(outs());
 	const std::vector<Loop *> &TopLoops = li.getTopLevelLoops();
 
-	std::vector<Loop *> Candidates;
+	/* Collect candidates */
+	std::set<Loop *> Candidates;
 	for (Loop *L : TopLoops)
 	{
 		//outs() << L->isLoopSimplifyForm() << "\n";
 		if (L->isLoopSimplifyForm() && loopContainsVolatileInst(*L) == false)
 		{
-			Candidates.push_back(L);
+			Candidates.insert(L);
 		}
+	}
+
+	DominatorTree		DT(F);
+	PostDominatorTree	PDT(F);
+	
+	/* Build Control Flow Equivalent sets */
+	std::vector<std::vector<Loop *>> CFEs;
+	for (auto it = Candidates.begin(), ite = Candidates.end(); it != ite; ++it)
+	{
+		//std::vector A;
+		//A.push_back(*it);
+		CFEs.push_back(std::vector<Loop *> {*it});
+	}
+	for (auto it1 = CFEs.begin(), it1e = CFEs.end(); it1 != it1e; ++it1)
+	{
+		Loop *L1 = (*it1)[0];
+		
+		for (auto it2 = Candidates.begin(), it2e = Candidates.end(); it2 != it2e;)
+		{
+			Loop *L2 = *it2;
+			if (loopDominates(L1, L2, DT) && loopPostDominates(L2, L1, PDT))
+			{
+				outs() << "	yes\n";
+				it1->push_back(L2);
+				it2 = Candidates.erase(it2);
+			}
+			else
+			{
+				++it2;
+			}
+		}
+	}
+	CFEs.erase(
+		std::remove_if(
+			CFEs.begin(),
+			CFEs.end(),
+			[](const std::vector<Loop *> v)
+			{
+				return v.size() == 1;
+			}
+		),
+		CFEs.end()
+	);
+	outs() << CFEs.size() << "__\n";
+
+	for (auto &set : CFEs)
+	{
+		
 	}
 
 	bool fused = false;

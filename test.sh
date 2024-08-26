b6a988a18590ad0ea046b1236ef52110d3f870c9
Andrei <95971066+RomanovAG@users.noreply.github.com>
2024-08-27 02:49:48 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Set paths to the necessary binaries and files
 CLANG="./../build/bin/clang-20"
 OPT="./../build/bin/opt"
 INPUT_FILE="./tests/fusion-manyloops-input.c"
 PLUGIN_PATH="./build/libfusion-pass.so"
 
-# Output files
 EXE_INPUT="fusion-manyloops-input.exe"
 EXE_OUTPUT="fusion-manyloops-output.exe"
 LL_INPUT="fusion-manyloops-input.ll"
@@ -16,14 +14,12 @@ DEBUG_FILE="debug.txt"
 
 > $DEBUG_FILE
 
-# Compile the first program
 $CLANG -O0 -Xclang -disable-O0-optnone $INPUT_FILE -o $EXE_INPUT
 if [ $? -ne 0 ]; then
     echo "First program compilation failed."
     exit 1
 fi
 
-# Compile the second program: generate LLVM IR, run mem2reg and the custom pass
 $CLANG -O0 -Xclang -disable-O0-optnone -S -emit-llvm $INPUT_FILE -o $LL_INPUT
 if [ $? -ne 0 ]; then
     echo "LLVM IR generation failed."
@@ -48,7 +44,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Compare the outputs
 ./$EXE_INPUT > res_input.txt
 ./$EXE_OUTPUT > res_output.txt
 diff res_input.txt res_output.txt > diff_output.txt
@@ -56,7 +51,7 @@ diff res_input.txt res_output.txt > diff_output.txt
 rm $EXE_INPUT
 rm $EXE_OUTPUT
 
-if [ $? -eq 0 ]; then
+if [ ! -s diff_output.txt ]; then
     echo "Test passed: The outputs are identical."
 else
     echo "Test failed: The outputs differ. Check diff_output.txt for details."
